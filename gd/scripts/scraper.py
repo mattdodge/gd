3073564267beb16043f6c8c96619bafddfe8cfe2
briancurtin <brian@python.org>
2014-07-22 16:28:12 -0500

    Minor refactorings in scraper

--- gd/scripts/scraper.py
+++ gd/scripts/scraper.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 from datetime import datetime, timedelta
 from urllib.parse import urljoin
 import argparse
@@ -23,6 +21,39 @@ def do_parse():
     pass
 
 
+def _get_request_range(begin, end):
+    if begin.date is None:
+        start = scrape.WEB_ROOT
+    else:
+        start = urljoin(scrape.WEB_ROOT,
+                        utils.datetime_to_url(begin.date, begin.num_parts))
+
+    if end.date is None:
+        stop = urljoin(scrape.WEB_ROOT, utils.datetime_to_url(
+                       datetime.today() - timedelta(days=1)))
+    else:
+        stop = urljoin(scrape.WEB_ROOT,
+                       utils.datetime_to_url(end.date, end.num_parts))
+
+    return start, stop
+
+
+def _get_file_list(session, begin, end):
+    all_years = scrape.get_years(session=session)
+    inc_years = utils.get_inclusive_urls(all_years, begin, end)
+
+    all_months = scrape.get_months(inc_years, session=session)
+    inc_months = utils.get_inclusive_urls(all_months, begin, end)
+
+    all_days = scrape.get_days(inc_months, session=session)
+    inc_days = utils.get_inclusive_urls(all_days, begin, end)
+
+    games = scrape.get_games(inc_days, session=session)
+    files = scrape.get_files(games, session=session)
+
+    return files
+
+
 def do_scrape(cache, begin=None, end=None):
     """Run the scraper over the range [begin, end]
 
@@ -35,32 +66,11 @@ def do_scrape(cache, begin=None, end=None):
     begin = utils.get_boundary(begin)
     end = utils.get_boundary(end)
 
-    if begin is None:
-        start = scrape.WEB_ROOT
-    else:
-        start = urljoin(scrape.WEB_ROOT,
-                        scrape.datetime_to_url(begin.date, begin.num_parts))
-
-    if end is None:
-        stop = urljoin(scrape.WEB_ROOT, scrape.datetime_to_url(
-                       datetime.today() - timedelta(days=1)))
-    else:
-        stop = urljoin(scrape.WEB_ROOT,
-                       scrape.datetime_to_url(end.date, end.num_parts))
-
     session = requests.Session()
 
-    all_years = scrape.get_years(session=session)
-    inc_years = utils.get_inclusive_urls(all_years, start, stop)
-
-    all_months = scrape.get_months(inc_years, session=session)
-    inc_months = utils.get_inclusive_urls(all_months, start, stop)
-
-    all_days = scrape.get_days(inc_months, session=session)
-    inc_days = utils.get_inclusive_urls(all_days, start, stop)
-
-    games = scrape.get_games(inc_days, session=session)
-    files = scrape.get_files(games, session=session)
+    start, stop = _get_request_range(begin, end)
+    files = _get_file_list(session, start, stop)
+    print(list(files)[0])
 
     count, fails = scrape.download(files, cache)
     end_scrape = datetime.now()
@@ -69,6 +79,8 @@ def do_scrape(cache, begin=None, end=None):
     if fails:
         for url in fails:
             log.error("failed to download %s", url)
+
+    return count
 
 
 def get_args():
